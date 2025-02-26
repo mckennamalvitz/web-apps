989e898b01d385535f739cd341719883d5ad8bdf
McKenna Malvitz <mckenna.malvitz@kellogg.northwestern.edu>
2025-02-26 01:30:38 +0000

    end of class

--- app/controllers/tacos_controller.rb
+++ app/controllers/tacos_controller.rb
@@ -1,7 +1,8 @@
 class TacosController < ApplicationController
 
   def index
-    render :inline => "<h1>Hello, world!<h1>"
+    #render :inline => "<h1>Hello, world!<h1>"
+    render :template => "tacos/index"
   end
 
 end
